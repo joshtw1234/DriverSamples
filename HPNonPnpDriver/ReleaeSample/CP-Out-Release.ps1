41cb67bef61d56b79d2ca312ef73067cb1da22c1
Josh Tseng <josh.tseng@hp.com>
2025-07-08 11:38:41 +0800

    update release.

--- HPNonPnpDriver/ReleaeSample/CP-Out-Release.ps1
+++ HPNonPnpDriver/ReleaeSample/CP-Out-Release.ps1
@@ -18,12 +18,15 @@ else{
 	$RROOT='1-JoshDev'
 }
 
-xcopy /Y/D/S/E ..\exe\x64\$DebugROOT\*.exe .
-xcopy /Y/D/S/E ..\sys\x64\$DebugROOT\HPNonPnpDriver\*.sys .
-xcopy /Y/D/S/E ..\exe\x64\$DebugROOT\*.pdb .
-xcopy /Y/D/S/E ..\sys\x64\$DebugROOT\*.pdb .
-xcopy /Y/D/S/E "C:\Program Files (x86)\Windows Kits\8.0\redist\wdf\x64\WdfCoInstaller01011.dll" .
-xcopy /Y/D/S/E ..\exe\x64\$DebugROOT\*.inf .
+$PARAM='/Y/D/S'
 
-xcopy /Y/D/S/E . \\$RemotIP\$RROOT\HPDriver\z-HPNonPnpDriver\
+
+xcopy $PARAM ..\exe\x64\$DebugROOT\*.exe .
+xcopy $PARAM ..\sys\x64\$DebugROOT\HPNonPnpDriver\*.sys .
+xcopy $PARAM ..\exe\x64\$DebugROOT\*.pdb .
+xcopy $PARAM ..\sys\x64\$DebugROOT\*.pdb .
+xcopy $PARAM "C:\Program Files (x86)\Windows Kits\8.0\redist\wdf\x64\WdfCoInstaller01011.dll" .
+xcopy $PARAM ..\exe\x64\$DebugROOT\*.inf .
+
+xcopy $PARAM . \\$RemotIP\$RROOT\HPDriver\z-HPNonPnpDriver\
 pause
